a55c4c78d838635b930321f380412ec0db0b4f66
elchinovzla <sonnyng.vzla@gmail.com>
2024-01-24 12:53:41 -0700

    setup bootstrap project with react + typescript + webpack

---
+++ config/webpack.dev.js
@@ -0,0 +1,16 @@
+const { merge } = require('webpack-merge');
+const commonConfig = require('./webpack.common');
+
+const devConfig = {
+  mode: 'development',
+  output: {
+    publicPath: 'http://localhost:8080/',
+  },
+  devServer: {
+    port: 8080,
+    historyApiFallback: true,
+    hot: true,
+  },
+};
+
+module.exports = merge(commonConfig, devConfig);
