a02ca1e45ac03ba851ca4f1267c4b65c5a4a4415
elchinovzla <sonnyng.vzla@gmail.com>
2024-01-26 12:13:49 -0700

    update rollup

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import postcss from 'rollup-plugin-postcss';
 import typescript from 'rollup-plugin-typescript2';
 const packageJson = require('./package.json');
 export default {
-  input: 'src/components/index.tsx',
+  input: 'src/components/index.ts',
   output: [
     {
       file: packageJson.main,
