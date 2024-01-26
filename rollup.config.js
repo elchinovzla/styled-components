e3b121af818cfd4ea770f8d8a55e4c2ff256ee10
elchinovzla <sonnyng.vzla@gmail.com>
2024-01-26 12:22:36 -0700

    update the export in index

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import postcss from 'rollup-plugin-postcss';
 import typescript from 'rollup-plugin-typescript2';
 const packageJson = require('./package.json');
 export default {
-  input: 'src/components/index.ts',
+  input: 'src/index.ts',
   output: [
     {
       file: packageJson.main,
